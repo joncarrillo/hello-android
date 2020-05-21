5d7ddc3d1702edab0f2aa41e0d82e90a816f5a2c
Jonatan Carrillo <jonatanfrancisco.carrillochavez@mercadolibre.com.mx>
2020-05-21 05:45:00 -0500

    Basic login interaction & redirect

--- app/src/main/java/me/jfcarrillo/helloworldjfc/MainActivity.kt
+++ app/src/main/java/me/jfcarrillo/helloworldjfc/MainActivity.kt
@@ -2,6 +2,12 @@ package me.jfcarrillo.helloworldjfc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +15,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setContentView(R.layout.activity_login)
+
+        val username = findViewById<EditText>(R.id.username)
+        val password = findViewById<EditText>(R.id.password)
+        val login = findViewById<Button>(R.id.login)
+        val loading = findViewById<ProgressBar>(R.id.loading)
+
+        login.isEnabled = true
+
+        login.setOnClickListener {
+            loading.visibility = View.VISIBLE
+
+            if (username.text.toString() == "jfc" && password.text.toString() == "123") {
+                Toast.makeText(applicationContext, "Correcto", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this,ProductsActivity::class.java))
+                finish()
+            } else {
+                Toast.makeText(applicationContext, "Error", Toast.LENGTH_SHORT).show()
+                loading.visibility = View.GONE
+            }
+        }
+
+
     }
 }
